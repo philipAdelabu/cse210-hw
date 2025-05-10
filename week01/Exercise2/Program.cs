8b59c5bd1fc849edca589109068913411acf803c
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-10 11:36:19 +0100

    Adding the first requirement to the exercise 2 of week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your percentage grade: ");
+        string percentage = Console.ReadLine();
+        int p_grade = int.Parse(percentage);
+        string message = "Your grade is : ";
+        if (p_grade >= 90){
+            Console.WriteLine($"{message } A");
+        }else if(p_grade >= 80 && p_grade < 90){
+            Console.WriteLine($"{message} B");
+        }else if(p_grade >= 70 && p_grade < 80){
+            Console.WriteLine($"{message} C");
+        }else if(p_grade >= 60 && p_grade < 70){
+            Console.WriteLine($"{message} D");
+        }else{
+            Console.WriteLine($"{message} F");
+        }
+            
     }
 }
