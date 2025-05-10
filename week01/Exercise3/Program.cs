85eb7daac22cce14f785a9938aa824f12fc76332
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-10 14:33:51 +0100

    Created the number guess game for exercise 3 in week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is your magic number?");
+        int magic_number = int.Parse(Console.ReadLine());
+        string next = "yes";
+        int attempt = 1;
+        while(next == "yes" ){
+            Console.Write("What is your guess?");
+            int guess = int.Parse(Console.ReadLine());
+            if(guess > magic_number){
+                  Console.WriteLine("Your guess is high, try a lower number");
+            }else if(guess < magic_number){
+                 Console.WriteLine("Your guess is low, try a higher one.");
+            }else{
+                Console.WriteLine("You guessed it !!");
+                Console.WriteLine("Would you like to play again (yes|no) ?");
+               next = Console.ReadLine();
+                if(next == "no")
+                   break;
+            }
+            
+            attempt++;
+        }
+        Console.WriteLine($"Number of attemp: {attempt}");
     }
 }
