fa0998debd791cdbf399fd1d17a23c93e992e8a6
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-10 17:33:11 +0100

    List programming modification for exercise 4 week1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        while(number != 0){
+            numbers.Add(number);
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+       int sum = 0, largest = numbers[0];
+       int smallest = largest;
+        sum += numbers[0];
+        for(int index = 1; index < numbers.Count; index++){
+            int num = numbers[index];
+            if(largest < num) 
+                 largest = num;
+            if(smallest > num && num > 0)
+                smallest = num;
+         sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum/numbers.Count}");
+        Console.WriteLine($"The largest is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+        Console.WriteLine($"The sorted list is: ");
+        numbers.Sort();
+        foreach(int num in numbers)
+                 Console.WriteLine(num);
     }
 }
