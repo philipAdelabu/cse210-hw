91333f7be16bb36599aa55e9fe280ae8fc78096b
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-10 18:25:07 +0100

    Function assignment done for exercise5 week 1

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        DisplayResult(name, SquareNumber(number));
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+        return ;
+    }
+    static string PromptUserName(){
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Enter your favourite number : ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number){
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number){
+        Console.WriteLine($"{name}, the square of your number is {number}");
+        return;
     }
 }
