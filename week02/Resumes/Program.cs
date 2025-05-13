513757944a2f90767cc6eb4527d15ac881f1619e
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-13 16:58:25 +0100

    Both Job and Resume class modified

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,11 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        Job job = new Job();
+        job._company = "Microsoft";
+        job._jobTitle = "Software";
+        job._endYear = "2023";
+        job._startYear = "1990"; 
+        job.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Jonh Doe";
         Job job1 = new Job();
-        job1._company = "Microsoft";
-        job1._jobTitle = "Software";
-        job1._endYear = "2023";
-        job1._startYear = "1990"; 
-        job1.Display();
+        job1._company = "Apple";
+        job1._jobTitle = "Engineer";
+        job1._startYear = "2001";
+        job1._endYear = "2020";
+        resume._jobs.Add(job1);
+
+        Job job2 = new Job();
+        job2._company = "Stripe";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = "2012";
+        job2._endYear = "2021";
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -1,22 +1,11 @@
 public class Resume{
     public string _name;
-    public List<Job> jobs = new List<Job>();
+    public List<Job> _jobs = new List<Job>();
     
-    _name = "Joe Donw";
-    jobs[0]._company = "Apple";
-    jobs[0]._jobTitle = "Engineer";
-    jobs[0]._startYear = "2001";
-    jobs[0]._endYear = "2020";
-
-    jobs[0]._company = "Stripe";
-    jobs[0]._jobTitle = "Software Developer";
-    jobs[0]._startYear = "2012";
-    jobs[0]._endYear = "2021";
-
-
     public void Display(){
-        Console.WriteLine(_name)
-        foreach(Job job in Jobs){
+        Console.WriteLine("Name: " + _name);
+        Console.WriteLine("Jobs: ");
+        foreach(Job job in _jobs){
             job.Display();
         }
     }
