96914c7acfdab6300c9da7c5a935bad0e53af9bf
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-16 18:47:50 +0100

    Added some class stubs as a skeleton

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,15 @@
+public class Journal {
+
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry){
+        return;
+    }
+
+    public void SaveToFile(string file){
+        return;
+    }
+    public void LoadFromFile(string file){
+        return;
+    }
+}
